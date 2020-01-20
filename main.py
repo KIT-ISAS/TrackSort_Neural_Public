787543030eef92735c8dd044b0d54eea58632ad7
sidney <sb.sidney.bender@gmail.com>
2020-01-20 19:32:21 +0100

    the first step towards an automated hyperparameter search are done now

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 
 from moviepy.editor import ImageSequenceClip
 from data_association import DataAssociation
+from evaluator import Evaluator
 
 parser = argparse.ArgumentParser()
 
@@ -13,12 +14,12 @@ parser.add_argument('--model_path', default='models/rnn_model_fake_data.h5', hel
 parser.add_argument('--matching_algorithm', default='local', help='The algorithm, that is used for matching. Current options are: ["local","global"])')
 parser.add_argument('--dataset_dir', default='data/Pfeffer/trackSortResultPfeffer/*_trackHistory_NothingDeleted.csv', help='The directory of the data set. Only needed for CsvDataset.')
 parser.add_argument('--dataset_type', default='CsvDataset', help='The type of the dataset. Current options are: ["FakeDataset","CsvDataset"].')
-parser.add_argument('--distance_threshold', type=float, default=0.03, help='The threshhold, that is used for the matching with the artificial measurements and predictions')
+parser.add_argument('--distance_threshold', type=float, default=0.01, help='The threshhold, that is used for the matching with the artificial measurements and predictions')
 parser.add_argument('--batch_size', type=int, default=64, help='The batchsize, that is used for training and inference')
-parser.add_argument('--num_timesteps', type=int, default=359, help='The number of timesteps of the dataset. Necessary for FakeDataset.')
+parser.add_argument('--num_timesteps', type=int, default=20, help='The number of timesteps of the dataset. Necessary for FakeDataset.')
 parser.add_argument('--num_train_epochs', type=int, default=1000, help='Only necessary, when model is trained.')
 parser.add_argument('--nan_value', type=float, default=0.0, help='The Nan value, that is used by the DataManager')
-parser.add_argument('--birth_rate_mean', type=float, default=1.0, help='The birth_rate_mean value, that is used by the DataManager')
+parser.add_argument('--birth_rate_mean', type=float, default=10.0, help='The birth_rate_mean value, that is used by the DataManager')
 parser.add_argument('--birth_rate_std', type=float, default=2.0, help='The birth_rate_std value, that is used by the DataManager')
 parser.add_argument('--min_number_detections', type=int, default=6, help='The min_number_detections value, that is used by the DataManager')
 parser.add_argument('--input_dim', type=int, default=2, help='The input_dim value, that is used by the DataManager')
@@ -54,88 +55,56 @@ global_config = {
     },
     #
     'num_train_epochs': args.num_train_epochs,
-    'visualization_path': 'visualizations/matching_visualization_local/',
+    'visualization_path': 'visualizations/matching_visualization/',
+    'best_visualization_path': 'visualizations/best_matching_visualization/',
     'visualization_video_path': 'visualizations/matching_visualization_vid.mp4',
+    'best_visualization_video_path': 'visualizations/best_matching_visualization_vid.mp4',
     'state_overwriting_started': False,
-    'overwriting_activated': False
+    'overwriting_activated': False,
+    'verbos': 0,
+    'visualize' : False,
+    'run_hyperparameter_search' : True,
+    'debug' : False
 }
 
-data_association = DataAssociation(global_config)
 
-tracks = data_association.associate_data()
-particles = data_association.data_source.get_particles()
+def run_global_config(global_config):
+	#
+	data_association = DataAssociation(global_config)
+	particles = data_association.data_source.get_particles()
+	tracks = data_association.associate_data()
+	#
+	if global_config['visualize']:
+		shutil.rmtree(global_config['visualization_video_path'], ignore_errors=True)
+		clip = ImageSequenceClip(global_config['visualization_path'], fps=4)
+		clip.write_videofile(global_config['visualization_video_path'], fps=4)
+	#
+	evaluator = Evaluator(global_config, particles, tracks)
+	accuracy_of_the_first_kind = 1.0 - evaluator.error_of_first_kind()
+	#accuracy_of_the_second_kind = 1.0 - evaluator.error_of_second_kind()
+	accuracy_of_the_second_kind = accuracy_of_the_first_kind
+	score = 2 * accuracy_of_the_first_kind * accuracy_of_the_second_kind / (accuracy_of_the_first_kind + accuracy_of_the_second_kind)
+	return score, accuracy_of_the_first_kind, accuracy_of_the_second_kind
 
-shutil.rmtree(global_config['visualization_video_path'], ignore_errors=True)
-clip = ImageSequenceClip(global_config['visualization_path'], fps=4)
-clip.write_videofile(global_config['visualization_video_path'], fps=4)
+dt = global_config['distance_threshold']
+best_score = 0.0
+distance_threshold_candidates = [0.25 * dt, 0.5 * dt, dt, 2.0 * dt, 4.0 * dt]
+dtc_scores = []
+best_dtc = distance_threshold_candidates[0]
+for dtc in distance_threshold_candidates:
+	print('run distance_threshhold ' + str(dtc))
+	global_config['distance_threshold'] = dtc
+	current_score, accuracy_of_the_first_kind, accuracy_of_the_second_kind = run_global_config(global_config)
+	dtc_scores.append([current_score, accuracy_of_the_first_kind, accuracy_of_the_second_kind])
+	print(str([current_score, accuracy_of_the_first_kind, accuracy_of_the_second_kind]))
+	if current_score > best_score:
+		best_score = current_score
+		best_dtc = dtc
+		if global_config['visualize']:
+			shutil.move(global_config['visualization_video_path'], global_config['best_visualization_video_path'])
+			shutil.move(global_config['visualization_path'], global_config['best_visualization_path'])
+global_config['distance_threshold'] = best_dtc
+
 
 print('data association finished!')
 code.interact(local=dict(globals(), **locals()))
-
-
-# type of errors
-# error of first kind: track contains multiple particles
-def assigment_of_measurement_in_particle(timestep, measurement):
-    for idx, particle_list in enumerate(particles):
-        for particle in particle_list:
-            particle_timestep, particle_measurement = particle
-            timestep = particle_timestep # TODO fix particle timestep extraction
-            if timestep == particle_timestep and particle_measurement[0] == measurement[0] and particle_measurement[1] == measurement[1]:
-                print(str(timestep) + ' - ' + str(measurement) + ' - measurement found match in particles! - ' + str(idx))
-                return idx
-    print(str(timestep) + ' - ' + str(measurement) + ' - measurement found no match in particles! Probably a artificial measurement.')
-    # code.interact(local=dict(globals(), **locals()))
-    return -1
-
-
-#
-num_errors_of_first_kind = 0
-for track_id in list(tracks.keys()):
-    print(track_id)
-    track = tracks[track_id]
-    # code.interact(local=dict(globals(), **locals()))
-    particle_id = assigment_of_measurement_in_particle(track.initial_timestep, track.measurements[0])
-    def correct_condition(x):
-        particle_id_current = assigment_of_measurement_in_particle(x[0], x[1])
-        return particle_id_current != particle_id and particle_id_current != -1
-    check_list = []
-    for it, measurement in enumerate(track.measurements):
-        check_list.append([track.initial_timestep + it, measurement])
-    num_errors_of_first_kind += int(len(list(filter(correct_condition, check_list))) != 0)
-
-
-ratio_error_of_first_kind = num_errors_of_first_kind / len(tracks)
-print('ratio_error_of_first_kind: ' + str(ratio_error_of_first_kind))
-
-# type of errors
-# error of first kind: track contains multiple particles
-def assigment_of_measurement_in_track(timestep, measurement):
-    for idx in list(tracks.keys()):
-        track = tracks[idx]
-        for it, track_measurement in enumerate(track.measurements):
-            track_timestep = track.initial_timestep + it
-            timestep = track_timestep # TODO fix particle timestep extraction
-            if track_timestep == timestep and track_measurement[0] == measurement[0] and track_measurement[1] == measurement[1]:
-                return idx
-    else:
-        # print('measurement found no match in tracks!')
-        # code.interact(local=dict(globals(), **locals()))
-        return -1
-
-
-#
-num_errors_of_second_kind = 0
-for particle_id, particle_list in enumerate(particles):
-    track_id = assigment_of_measurement_in_track(particle_list[0][0], particle_list[0][1])
-    def correct_condition(x):
-        track_id_current = assigment_of_measurement_in_track(x[0], x[1])
-        return track_id_current != track_id and track_id_current != -1
-    check_list = []
-    for it, particle in enumerate(particle_list):
-        check_list.append([particle[0], particle[1]])
-    num_errors_of_second_kind += int(len(list(filter(correct_condition, check_list))) != 0)
-
-
-ratio_error_of_second_kind = num_errors_of_second_kind / len(tracks)
-print('ratio_error_of_second_kind: ' + str(ratio_error_of_second_kind))
-
