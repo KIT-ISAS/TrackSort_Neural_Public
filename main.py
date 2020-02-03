bc8f3b7ff84cb5267d5dde201ace6cac120aaa5f
Daniel Pollithy <pollithy.daniel@gmail.com>
2020-02-03 19:08:51 +0100

    tensorflow log level

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ import json
 import datetime
 import logging
 
+import tensorflow as tf
+
 from moviepy.editor import ImageSequenceClip
 from data_association import DataAssociation
 from evaluator import Evaluator
 
+tf.get_logger().setLevel('ERROR')
 parser = argparse.ArgumentParser()
 
 
