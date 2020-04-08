dfdafe17c710bfa6ea7b95716ba2067a964b3c6f
Daniel Pollithy <pollithy.daniel@gmail.com>
2020-04-08 15:36:29 +0200

    delete bash files

--- data_association_on_DEM.sh
+++
@@ -1,3 +0,0 @@
-python main.py --model_path "models/DEM_model.h5" --dataset_dir "data/DEM_cylinder.csv" \
---data_is_aligned False --is_loaded True  --rotate_columns True --run_hyperparameter_search True  \
---normalization_constant 1.0 --distance_threshold 0.01 --matching_algorithm global
--- data_association_on_DEM_noise_robustness.sh
+++
@@ -1,3 +0,0 @@
-python main.py --model_path "models/DEM_model.h5" --dataset_dir "data/DEM_cylinder.csv" \
-	--data_is_aligned False --is_loaded True  --rotate_columns True --run_hyperparameter_search False  \
-	--normalization_constant 1.0 --test_noise_robustness True --distance_threshold 0.02
