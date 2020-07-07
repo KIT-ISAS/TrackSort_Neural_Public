c44964498ad672da647f9245e9cfdb8ea03cd567
Daniel Pollithy <pollithy.daniel@gmail.com>
2020-07-07 15:10:22 +0200

    Additive noise test for MC Dropout

---
+++ additive_noise__mc_dropout.sh
@@ -0,0 +1,80 @@
+python main.py \
+     --is_loaded True \
+     --model_path "models/mc4.h5" \
+     --dataset_dir "data/DEM_cylinder.csv" \
+     --data_is_aligned False \
+     --normalization_constant 1.0 \
+     --rotate_columns True \
+     --additive_noise_stddev 0.0 \
+     --mc_dropout True \
+     --distance_confidence 0.999 \
+     --calibrate False \
+     --description "Noise=0. Calibrate=False. MC-Drop."
+
+python main.py \
+     --is_loaded True \
+     --model_path "models/mc4.h5" \
+     --dataset_dir "data/DEM_cylinder.csv" \
+     --data_is_aligned False \
+     --normalization_constant 1.0 \
+     --rotate_columns True \
+     --additive_noise_stddev 0.0 \
+     --mc_dropout True \
+     --distance_confidence 0.999 \
+     --calibrate True \
+     --description "Noise=0. Calibrate=True. MC-Drop."
+
+python main.py \
+     --is_loaded True \
+     --model_path "models/mc4.h5" \
+     --dataset_dir "data/DEM_cylinder.csv" \
+     --data_is_aligned False \
+     --normalization_constant 1.0 \
+     --rotate_columns True \
+     --additive_noise_stddev 0.0003 \
+     --mc_dropout True \
+     --distance_confidence 0.999 \
+     --calibrate True \
+     --description "Noise=0.0003 Calibrate=True. MC-Drop."
+
+
+python main.py \
+     --is_loaded True \
+     --model_path "models/mc4.h5" \
+     --dataset_dir "data/DEM_cylinder.csv" \
+     --data_is_aligned False \
+     --normalization_constant 1.0 \
+     --rotate_columns True \
+     --additive_noise_stddev 0.0005 \
+     --mc_dropout True \
+     --distance_confidence 0.999 \
+     --calibrate True \
+     --description "Noise=0.0005 Calibrate=True. MC-Drop."
+
+
+python main.py \
+     --is_loaded True \
+     --model_path "models/mc4.h5" \
+     --dataset_dir "data/DEM_cylinder.csv" \
+     --data_is_aligned False \
+     --normalization_constant 1.0 \
+     --rotate_columns True \
+     --additive_noise_stddev 0.0008 \
+     --mc_dropout True \
+     --distance_confidence 0.999 \
+     --calibrate True \
+     --description "Noise=0.0008 Calibrate=True. MC-Drop."
+
+
+python main.py \
+     --is_loaded True \
+     --model_path "models/mc4.h5" \
+     --dataset_dir "data/DEM_cylinder.csv" \
+     --data_is_aligned False \
+     --normalization_constant 1.0 \
+     --rotate_columns True \
+     --additive_noise_stddev 0.001 \
+     --mc_dropout True \
+     --distance_confidence 0.999 \
+     --calibrate True \
+     --description "Noise=0.001 Calibrate=True. MC-Drop."
